705c12cfa48e8f13e68640150842f92b6c49500a
Talo Vargas <166659393+govargas@users.noreply.github.com>
2025-09-14 11:39:24 +0200

    feat(frontend): add routes for list and beach detail

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import BeachesList from "./components/BeachesList";
+import BeachDetailPage from "./pages/BeachDetailPage";
 
 export default function App() {
-  return <BeachesList />;
+  return (
+    <Routes>
+      <Route path="/" element={<BeachesList />} />
+      <Route path="/beach/:id" element={<BeachDetailPage />} />
+    </Routes>
+  );
 }
