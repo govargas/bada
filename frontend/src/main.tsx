797669c67d0aa9a2c28c4d3dd7aed6ad39e93f34
Talo Vargas <166659393+govargas@users.noreply.github.com>
2025-09-14 21:43:49 +0200

    chore(frontend): import Tailwind CSS in main entry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import "./i18n";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
